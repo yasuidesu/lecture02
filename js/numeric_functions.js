700f4f499d606cfb6adac8d513a06ad4dd6f0799
yasuidesu <yasui44@outlook.jp>
2015-10-22 12:35:37 +0900

    練習問題３、４、

--- js/numeric_functions.js
+++ js/numeric_functions.js
@@ -17,3 +17,17 @@ function divide(valueA, valueB){
 function mod(valueA, valueB){
   return valueA % valueB;
 }
+
+
+
+
+function circumfercence(r){
+  if (r >= 0){
+    return 2 * r *3;
+  }
+  return 0;
+}
+
+function circleArea(r){
+  return Math.pow(r,2)*Math.PI;
+}
